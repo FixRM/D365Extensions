a94c034872840f3141b8ff28e735082006fbf64c
Artem Grunin <agrunin@gmcs.ru>
2018-12-19 15:08:02 +0500

    Add EntityReference extensions

---
+++ D365Extensions/D365Extensions/EntityReferenceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk
+{
+    public static class EntityReferenceExtensions
+    {
+        /// <summary>
+        /// Gets the entity based on the EntityReference
+        /// </summary>
+        /// <returns></returns>
+        public static Entity ToEntity(this EntityReference entityReference)
+        {
+            return new Entity()
+            {
+                Id = entityReference.Id,
+                LogicalName = entityReference.LogicalName,
+            };
+        }
+
+        /// <summary>
+        /// Gets the entity based on the EntityReference as the specified type
+        /// </summary>
+        /// <returns></returns>
+        public static T ToEntity<T>(this EntityReference entityReference) where T : Entity
+        {
+            return entityReference.ToEntity().ToEntity<T>();
+        }
+    }
+}
