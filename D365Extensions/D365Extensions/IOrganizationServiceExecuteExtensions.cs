0bb3433f3ff700f3371f4431319981d200704770
Artem Gruinin <FixRM@users.noreply.github.com>
2019-05-29 18:17:55 +0500

    Add IOrganizationService.Execute method override. Probably, the most useless extension here :)

---
+++ D365Extensions/D365Extensions/IOrganizationServiceExecuteExtensions.cs
@@ -0,0 +1,20 @@
+﻿using D365Extensions;
+
+namespace Microsoft.Xrm.Sdk
+{
+    /// <summary>
+    /// Extensions for IOrganizationService.Execute
+    /// </summary>
+    public static partial class IOrganizationServiceExtensions
+    {
+        /// <summary>
+        /// Execute method override. Returns OrganizationResponse as the specified type
+        /// </summary>
+        public static T Execute<T>(this IOrganizationService service, OrganizationRequest request) where T : OrganizationResponse
+        {
+            CheckParam.CheckForNull(request, nameof(request));
+
+            return service.Execute(request) as T;
+        }
+    }
+}
