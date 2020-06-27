5a098d4740c999e04fd8e4739390e7449ab2ee66
Artem Gruinin <FixRM@users.noreply.github.com>
2020-06-27 17:12:04 +0500

    chore: add property name reader for lambda expressions

--- D365Extensions/D365Extensions.Tests/TestEntity.cs
+++ D365Extensions/D365Extensions.Tests/TestEntity.cs
@@ -11,12 +11,45 @@ namespace D365Extensions.Tests
     /// <summary>
     /// Simple Entity inheritor for Test purpose
     /// </summary>
-    [EntityLogicalNameAttribute("fixrm_testentity")]
+    [EntityLogicalName("fixrm_testentity")]
     public class TestEntity : Entity
     {
-        public TestEntity() : base("fixrm_testentity")
-        {
+        public const string EntityLogicalName = "fixrm_testentity";
 
+        public TestEntity() : base(EntityLogicalName)
+        {
+        }
+
+        /// <summary>
+        /// Reference type property
+        /// </summary>
+        [AttributeLogicalName("referencetypeproperty")]
+        public string ReferenceTypeProperty
+        {
+            get
+            {
+                return this.GetAttributeValue<string>("referencetypeproperty");
+            }
+            set
+            {
+                this.SetAttributeValue("referencetypeproperty", value);
+            }
+        }
+
+        /// <summary>
+        /// Value type property
+        /// </summary>
+        [AttributeLogicalName("valuetypeproperty")]
+        public System.Nullable<int> ValueTypeProperty
+        {
+            get
+            {
+                return this.GetAttributeValue<System.Nullable<int>>("valuetypeproperty");
+            }
+            set
+            {
+                this.SetAttributeValue("valuetypeproperty", value);
+            }
         }
     }
 }
---
+++ D365Extensions/D365Extensions/ProperyExpression.cs
@@ -0,0 +1,45 @@
+﻿using D365Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace D365Extensions
+{
+    /// <summary>
+    /// Helper class for reading property names from lambda expressions
+    /// </summary>
+    public static class ProperyExpression
+    {
+        public static List<string> GetNames<T>(params Expression<Func<T, object>>[] expressions)
+        {
+            return expressions
+                .Select(e => GetName(e))
+                .ToList();
+        }
+
+        public static string GetName<T>(Expression<Func<T, object>> expression)
+        {
+            CheckParam.CheckForNull(expression, nameof(expression));
+
+            return GetName(expression.Body);
+        }
+
+        static string GetName(Expression expression)
+        {
+            // Property, field of method returning value type
+            if (expression is UnaryExpression unaryExpression)
+            {
+                expression = unaryExpression.Operand;
+            }
+
+            // Reference type property or field
+            if (expression is MemberExpression memberExpession)
+            {
+                return memberExpession.Member.Name.ToLower();
+            }
+
+            throw CheckParam.InvalidExpression(nameof(expression));
+        }
+    }
+}
