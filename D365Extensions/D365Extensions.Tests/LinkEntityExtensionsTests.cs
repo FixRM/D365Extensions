0590f900e464349ad32073f37df1d4b88987191a
Artem Gruinin <FixRM@users.noreply.github.com>
2020-07-01 16:53:46 +0500

    feat: add LinkEntityExtensions class

---
+++ D365Extensions/D365Extensions.Tests/LinkEntityExtensionsTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Query.Tests
+{
+    [TestClass()]
+    public class LinkEntityExtensionsTests
+    {
+        [TestMethod()]
+        public void AddLink1Test()
+        {
+            // Setup
+            string expectedFromEntityName = EntityFrom.EnityLogicalName;
+            string expectedToEntityName = EntityTo.EnityLogicalName;
+            string expectedFromAttributName = nameof(EntityFrom.FromId).ToLower();
+            string expectedToAttributName = nameof(EntityTo.ToId).ToLower();
+            JoinOperator expectedOperator = JoinOperator.LeftOuter;
+
+            LinkEntity linkEntity = new LinkEntity()
+            {
+                LinkFromEntityName = expectedFromEntityName
+            };
+
+            // Act
+            LinkEntity newLink = linkEntity.AddLink<EntityFrom, EntityTo>(
+                EntityTo.EnityLogicalName,
+                f=> f.FromId,
+                t=> t.ToId,
+                expectedOperator);
+
+            // Assert
+            Assert.AreEqual(expectedFromEntityName, newLink.LinkFromEntityName);
+            Assert.AreEqual(expectedToEntityName, newLink.LinkToEntityName);
+            Assert.AreEqual(expectedToAttributName, newLink.LinkToAttributeName);
+            Assert.AreEqual(expectedFromAttributName, newLink.LinkFromAttributeName);
+            Assert.AreEqual(expectedOperator, newLink.JoinOperator);
+        }
+
+        [TestMethod()]
+        public void AddLink2Test()
+        {
+            // Setup
+            string expectedFromEntityName = EntityFrom.EnityLogicalName;
+            string expectedToEntityName = EntityTo.EnityLogicalName;
+            string expectedFromAttributName = nameof(EntityFrom.FromId).ToLower();
+            string expectedToAttributName = nameof(EntityTo.ToId).ToLower();
+
+            LinkEntity linkEntity = new LinkEntity()
+            {
+                LinkFromEntityName = expectedFromEntityName
+            };
+
+            // Act
+            LinkEntity newLink = linkEntity.AddLink<EntityFrom, EntityTo>(
+                EntityTo.EnityLogicalName,
+                f => f.FromId,
+                t => t.ToId);
+
+            // Assert
+            Assert.AreEqual(expectedFromEntityName, newLink.LinkFromEntityName);
+            Assert.AreEqual(expectedToEntityName, newLink.LinkToEntityName);
+            Assert.AreEqual(expectedToAttributName, newLink.LinkToAttributeName);
+            Assert.AreEqual(expectedFromAttributName, newLink.LinkFromAttributeName);
+        }
+    }
+
+    public class EntityFrom : Entity
+    {
+        public static string EnityLogicalName = "EntityFrom";
+
+        public EntityFrom() :base(EnityLogicalName) 
+        {
+        }
+
+        public Guid FromId { get; set; }
+    }
+
+    public class EntityTo : Entity
+    {
+        public static string EnityLogicalName = "EntityTo";
+
+        public EntityTo() : base(EnityLogicalName)
+        {
+        }
+
+        public Guid ToId { get; set; }
+    }
+}
---
+++ D365Extensions/D365Extensions/LinkEntityExtentions.cs
@@ -0,0 +1,62 @@
+﻿using D365Extensions;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Query
+{
+    /// <summary>
+    /// Extensions for LinkEntity
+    /// </summary>
+    public static class LinkEntityExtensions
+    {
+        /// <summary>
+        /// Adds a link, setting the link to entity name, the link from attribute name and
+        /// the link to attribute name.
+        /// </summary>
+        /// <typeparam name="TFrom">Type of the entity to link from</typeparam>
+        /// <typeparam name="TTo">Type of the entity to link to</typeparam>
+        /// <param name="linkToEntityName">The name of the entity to link to.</param>
+        /// <param name="linkFromAttributeName">The property expressions containing the name of the attribute to link from.</param>
+        /// <param name="linkToAttributeName">The property expressions containing the name of the attribute to link to.</param>
+        /// <param name="joinOperator">The join operator.</param>
+        /// <returns>The link entity that was created.</returns>
+        public static LinkEntity AddLink<TFrom, TTo>(
+            this LinkEntity link,
+            string linkToEntityName,
+            Expression<Func<TFrom, object>> linkFromAttributeName,
+            Expression<Func<TTo, object>> linkToAttributeName,
+            JoinOperator joinOperator)
+        {
+            return link.AddLink(
+                linkToEntityName,
+                ProperyExpression.GetName<TFrom>(linkFromAttributeName),
+                ProperyExpression.GetName<TTo>(linkToAttributeName),
+                joinOperator);
+        }
+
+        /// <summary>
+        /// Adds a link, setting the link to entity name, the link from attribute name and
+        /// the link to attribute name.
+        /// </summary>
+        /// <typeparam name="TFrom">Type of the entity to link from</typeparam>
+        /// <typeparam name="TTo">Type of the entity to link to</typeparam>
+        /// <param name="linkToEntityName">The name of the entity to link to.</param>
+        /// <param name="linkFromAttributeName">The property expressions containing the name of the attribute to link from.</param>
+        /// <param name="linkToAttributeName">The property expressions containing the name of the attribute to link to.</param>
+        /// <returns>The link entity that was created.</returns>
+        public static LinkEntity AddLink<TFrom, TTo>(
+            this LinkEntity link,
+            string linkToEntityName,
+            Expression<Func<TFrom, object>> linkFromAttributeName,
+            Expression<Func<TTo, object>> linkToAttributeName)
+        {
+            return link.AddLink(
+                linkToEntityName,
+                ProperyExpression.GetName<TFrom>(linkFromAttributeName),
+                ProperyExpression.GetName<TTo>(linkToAttributeName));
+        }
+    }
+}
