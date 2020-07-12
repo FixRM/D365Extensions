7899dfafb23234b8ad0346e30d4d19b0b272d8d1
Artem Gruinin <FixRM@users.noreply.github.com>
2020-07-12 16:29:24 +0500

    feat: add QueryByAttributeExtensions

---
+++ D365Extensions/D365Extensions.Tests/QueryByAttributeExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    [TestClass()]
+    public class QueryByAttributeExtensionsTests
+    {
+        [TestMethod()]
+        public void AddAttributeTest()
+        {
+            // Setup
+            QueryByAttribute query = new QueryByAttribute();
+
+            var expectedAttributeName = nameof(TestEntity.ReferenceTypeProperty).ToLower();
+            var expectedAttributeValue = "not used";
+
+            // Act
+            query.AddAttribute<TestEntity>(t => t.ReferenceTypeProperty, expectedAttributeValue);
+
+            // Assert
+            Assert.AreEqual(1, query.Attributes.Count);
+
+            var actualAttribute = query.Attributes[0];
+            Assert.AreEqual(expectedAttributeName, actualAttribute);
+
+            var actualValue = query.Values[0];
+            Assert.AreEqual(expectedAttributeValue, actualValue);
+        }
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            // Setup
+            QueryByAttribute query = new QueryByAttribute();
+
+            var expectedAttributeName = nameof(TestEntity.ReferenceTypeProperty).ToLower();
+            var expectedOrder = OrderType.Ascending;
+
+            // Act
+            query.AddOrder<TestEntity>(t => t.ReferenceTypeProperty, expectedOrder);
+
+            // Assert
+            Assert.AreEqual(1, query.Orders.Count);
+
+            var actualOrder = query.Orders[0];
+            Assert.AreEqual(expectedAttributeName, actualOrder.AttributeName);
+            Assert.AreEqual(expectedOrder, actualOrder.OrderType);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/QueryByAttributeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using D365Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Query
+{
+    /// <summary>
+    /// Extensions for QueryByAttribute
+    /// </summary>
+    public static class QueryByAttributeExtensions
+    {
+        /// <summary>
+        /// Adds an attribute value to the attributes collection.
+        /// </summary>
+        /// <typeparam name="T">Type of the entity</typeparam>
+        /// <param name="attributeName">The property expressions containing the name of the attribute</param>
+        /// <param name="value">The attribute value.</param>
+        public static void AddAttribute<T>(this QueryByAttribute query, Expression<Func<T, object>> attributeName, object value) where T : Entity
+        {
+            query.AddAttributeValue(ProperyExpression.GetName(attributeName), value);
+        }
+
+        /// <summary>
+        /// Adds an order to the orders collection.
+        /// </summary>
+        /// <typeparam name="T">Type of the entity</typeparam>
+        /// <param name="attributeName">The property expressions containing the name of the attribute</param>
+        /// <param name="orderType">The order for that attribute.</param>
+        public static void AddOrder<T>(this QueryByAttribute query, Expression<Func<T, object>> attributeName, OrderType orderType) where T : Entity
+        {
+            query.AddOrder(ProperyExpression.GetName(attributeName), orderType);
+        }
+    }
+}
