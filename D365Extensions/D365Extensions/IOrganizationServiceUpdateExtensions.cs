fd52053b134bacad40f4edf3c6c4d15ca65a4035
Artem Gruinin <FixRM@users.noreply.github.com>
2020-07-19 19:08:29 +0500

    feat: add IOrganizationService.Update extensions

---
+++ D365Extensions/D365Extensions/IOrganizationServiceUpdateExtensions.cs
@@ -0,0 +1,27 @@
+﻿using D365Extensions;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+
+namespace Microsoft.Xrm.Sdk
+{
+    /// <summary>
+    /// Extensions for IOrganizationService.Update
+    /// </summary>
+    public static partial class IOrganizationServiceExtensions
+    {
+        /// <summary>
+        /// Update method override. Accept ConcurrencyBehavior parameter
+        /// </summary>
+        /// <param name="reference">Entity to delete</param>
+        public static void Update(this IOrganizationService service, Entity entity, ConcurrencyBehavior concurrencyBehavior)
+        {
+            CheckParam.CheckForNull(entity, nameof(entity));
+
+            service.Execute(new UpdateRequest()
+            {
+                Target = entity,
+                ConcurrencyBehavior = concurrencyBehavior
+            });
+        }
+    }
+}
