a4b68d7a1b1c70d5d4b80b96252a4a022c7ee260
Artem Gruinin <FixRM@users.noreply.github.com>
2021-10-02 13:59:49 +0500

    feat: add simple bencmark to measure reflection speed

---
+++ D365Extensions/D365Extensions.Benchmark/Program.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using D365Extensions.Tests;
+using D365Extensions.Tests.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Benchmark
+{
+    [MemoryDiagnoser]
+    public class PEBenchmark
+    {
+        [Benchmark]
+        public string LambdaRef() => ProperyExpression.GetName<CustomEntity>(e => e.Property_1);
+
+        [Benchmark]
+        public string LambdaVal() => ProperyExpression.GetName<CustomEntity>(e => e.Property_2);
+
+        [Benchmark]
+        public string ReflectionRef() => ProperyExpression.GetNameR<CustomEntity>(e => e.Property_1);
+
+        [Benchmark]
+        public string ReflectionVal() => ProperyExpression.GetNameR<CustomEntity>(e => e.Property_2);
+
+        [Benchmark]
+        public string ChacheRef() => ProperyExpression.GetNameC<CustomEntity>(e => e.Property_1);
+
+        [Benchmark]
+        public string ChacheVal() => ProperyExpression.GetNameC<CustomEntity>(e => e.Property_2);
+
+        [Benchmark]
+        public string Chache2Ref() => ProperyExpression.GetNameC2<CustomEntity>(e => e.Property_1);
+
+        [Benchmark]
+        public string Chache2Val() => ProperyExpression.GetNameC2<CustomEntity>(e => e.Property_2);
+    }
+
+    public class ToLowerBenchmark
+    {
+        [Benchmark]
+        public string ToLowerInvariant() => "AccountNumber".ToLowerInvariant();
+
+        [Benchmark]
+        public string ToLower() => "AccountNumber".ToLower();
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PEBenchmark>();
+            //var summary = BenchmarkRunner.Run<ToLowerBenchmark>();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions.Benchmark/ProperyExpression.cs
@@ -0,0 +1,149 @@
+﻿using D365Extensions;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace D365Extensions
+{
+    /// <summary>
+    /// Helper class for reading property names from lambda expressions
+    /// </summary>
+    public static class ProperyExpression
+    {
+        public static List<string> GetNames<T>(params Expression<Func<T, object>>[] expressions)
+        {
+            return expressions
+                .Select(e => GetName(e))
+                .ToList();
+        }
+
+        public static string GetName<T>(Expression<Func<T, object>> expression)
+        {
+            return GetName(expression?.Body);
+        }
+
+        public static string GetNameR<T>(Expression<Func<T, object>> expression)
+        {
+            return GetNameR(expression?.Body);
+        }
+
+        public static string GetNameC<T>(Expression<Func<T, object>> expression)
+        {
+            return GetNameC(expression?.Body);
+        }
+
+        static ConcurrentDictionary<Expression, string> expressionChache = new ConcurrentDictionary<Expression, string>();
+
+        public static string GetNameC2<T>(Expression<Func<T, object>> expression)
+        {
+            expressionChache.TryGetValue(expression, out string logicalName);
+
+            if (logicalName == null)
+            {
+                logicalName = GetNameC2(expression?.Body);
+                expressionChache.TryAdd(expression, logicalName);
+            }
+
+            return logicalName;
+        }
+
+        static string GetName(Expression expression)
+        {
+            if (expression == null) return null;
+
+            // Property, field of method returning value type
+            if (expression is UnaryExpression unaryExpression)
+            {
+                expression = unaryExpression.Operand;
+            }
+
+            // Reference type property or field
+            if (expression is MemberExpression memberExpession)
+            {
+                MemberInfo member = memberExpession.Member;
+
+                return member.Name.ToLowerInvariant();
+            }
+
+            throw new ArgumentException(nameof(expression));
+        }
+
+        static string GetNameR(Expression expression)
+        {
+            if (expression == null) return null;
+
+            // Property, field of method returning value type
+            if (expression is UnaryExpression unaryExpression)
+            {
+                expression = unaryExpression.Operand;
+            }
+
+            // Reference type property or field
+            if (expression is MemberExpression memberExpession)
+            {
+                MemberInfo member = memberExpession.Member;
+
+                return member.GetCustomAttribute<AttributeLogicalNameAttribute>(false).LogicalName;
+            }
+
+            throw new ArgumentException(nameof(expression));
+}
+
+        static ConcurrentDictionary<MemberInfo, string> memberChache = new ConcurrentDictionary<MemberInfo, string>();
+
+        static string GetNameC(Expression expression)
+        {
+            if (expression == null) return null;
+
+            // Property, field of method returning value type
+            if (expression is UnaryExpression unaryExpression)
+            {
+                expression = unaryExpression.Operand;
+            }
+
+            // Reference type property or field
+            if (expression is MemberExpression memberExpession)
+            {
+                MemberInfo member = memberExpession.Member;
+
+                memberChache.TryGetValue(member, out string logicalName);
+
+                if (logicalName == null)
+                {
+                    logicalName = member.GetCustomAttribute<AttributeLogicalNameAttribute>(false).LogicalName;
+
+                    memberChache.TryAdd(member, logicalName);
+                }
+
+                return logicalName;
+            }
+
+            throw new ArgumentException(nameof(expression));
+        }
+
+        static string GetNameC2(Expression expression)
+        {
+            if (expression == null) return null;
+
+            // Property, field of method returning value type
+            if (expression is UnaryExpression unaryExpression)
+            {
+                expression = unaryExpression.Operand;
+            }
+
+            // Reference type property or field
+            if (expression is MemberExpression memberExpession)
+            {
+                MemberInfo member = memberExpession.Member;
+
+                return member.GetCustomAttribute<AttributeLogicalNameAttribute>(false).LogicalName;
+            }
+
+            throw new ArgumentException(nameof(expression));
+        }
+    }
+}
