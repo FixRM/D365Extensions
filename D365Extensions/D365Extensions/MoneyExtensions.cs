2bcc2fd2edb0680df6793ef5a35bd887aa32586a
Artem Gruinin <FixRM@users.noreply.github.com>
2023-05-22 16:55:19 +0500

    feat: add ToTraceString extension to EntityReference, Money and OptionSetValue

---
+++ D365Extensions/D365Extensions/MoneyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace D365Extensions
+{
+    public static class MoneyExtensions
+    {
+        /// <summary>
+        /// Returns Money string representation
+        /// </summary>
+        /// <returns></returns>
+        public static string ToTraceString(this Money money)
+        {
+            return $"Money {{ Value = {money.Value} }}";
+        }
+    }
+}
