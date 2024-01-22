1a2c1f60aea7031192799940f6b52cf26ef00c48
Artem Gruinin <FixRM@users.noreply.github.com>
2024-01-22 17:35:57 +0500

    feat: add EntityImageCollectionExtensions

---
+++ D365Extensions/D365Extensions.Tests/EntityImageCollectionExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using D365Extensions.Tests.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    [TestClass()]
+    public class EntityImageCollectionExtensionsTests
+    {
+        [TestMethod()]
+        public void Should_Return_Null_Test()
+        {
+
+            // Setup
+            var images = new EntityImageCollection();
+
+            // Act
+            var nullImage = images.GetImage<Account>("PreImage");
+
+            // Assert
+            Assert.IsNull(nullImage);
+        }
+
+        [TestMethod()]
+        public void Should_Return_Image_Test()
+        {
+            // Setup
+            var image = new Entity()
+            {
+                Id = Guid.NewGuid(),
+                LogicalName = Account.EntityLogicalName,
+                [nameof(Account.Name).ToLower()] = "Test Account"
+            };
+
+            var expectedImageName = "PreImage";
+
+            var images = new EntityImageCollection
+            {
+                { expectedImageName, image }
+            };
+
+            // Act
+            var actualImage = images.GetImage<Account>(expectedImageName);
+
+            // Assert
+            Assert.IsNotNull(actualImage);
+
+            Assert.AreEqual(image.Id, actualImage.Id);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/EntityImageCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk
+{
+    public static class EntityImageCollectionExtensions
+    {
+        /// <summary>
+        /// Safely gets value from EntityImageCollection 
+        /// </summary>
+        /// <param name="name">image name</param>
+        /// <returns></returns>
+        public static Entity GetImage(this EntityImageCollection images, string name)
+        {
+            if (images.TryGetValue(name, out var image))
+            {
+                return image;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Safely gets value from EntityImageCollection 
+        /// </summary>
+        /// <typeparam name="T">Early Bound Entity type</typeparam>
+        /// <param name="name">image name</param>
+        /// <returns></returns>
+        public static T GetImage<T>(this EntityImageCollection images, string name) where T : Entity
+        {            
+            return images.GetImage(name)?.ToEntity<T>();
+        }
+    }
+}
