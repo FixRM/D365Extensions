b862ed04b3f34b9547475cf27112c6e7767f7d43
Artem Gruinin <FixRM@users.noreply.github.com>
2024-02-16 14:42:13 +0500

    feat: add public static ErrorCodes? GetErrorCode(this FaultException<OrganizationServiceFault> fault) extension method

---
+++ D365Extensions/D365Extensions.Tests/OrganizationServiceFaultExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    [TestClass()]
+    public class OrganizationServiceFaultExtensionsTests
+    {
+        [TestMethod()]
+        public void GetKnownErrorCodeTest()
+        {
+            // Setup
+            const ErrorCodes expectedErrorCode = ErrorCodes.AccessDenied;
+
+            var fault = new OrganizationServiceFault();
+            fault.ErrorCode = (int)expectedErrorCode;
+
+            var exception = new FaultException<OrganizationServiceFault>(fault);
+
+            // Act
+            var actualErrorCode = exception.GetErrorCode();
+
+            // Assert
+            Assert.IsNotNull(actualErrorCode);
+            Assert.AreEqual(expectedErrorCode, actualErrorCode);
+        }
+
+        [TestMethod()]
+        public void GetUnknownErrorCodeTest()
+        {
+            // Setup
+            var fault = new OrganizationServiceFault();
+            fault.ErrorCode = 42;
+
+            var exception = new FaultException<OrganizationServiceFault>(fault);
+
+            // Act
+            var error = exception.GetErrorCode();
+
+            // Assert
+            Assert.IsNull(error);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/OrganizationServiceFaultExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Microsoft.Xrm.Sdk
+{
+
+    /// <summary>
+    /// Converts OrganizatioService fault`s ErrorCode to ErrorCodes enumeration value.
+    /// 
+    /// Returns null if ErrorCode is unknown
+    /// </summary>
+    public static class OrganizationServiceFaultExtensions
+    {
+        public static ErrorCodes? GetErrorCode(this FaultException<OrganizationServiceFault> fault)
+        {
+            int code = fault.Detail.ErrorCode;
+
+            return Enum.IsDefined(typeof(ErrorCodes), code) ? (ErrorCodes?)code : null;
+        }
+    }
+}
