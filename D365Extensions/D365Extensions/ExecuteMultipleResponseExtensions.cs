c502e82120cbd072cb8dabd652c05e4ee95c8e2d
Artem Gruinin <FixRM@users.noreply.github.com>
2024-08-20 15:45:55 +0500

    refact: move ExecuteMultipleResponse extensions to it's own file

---
+++ D365Extensions/D365Extensions/ExecuteMultipleResponseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xrm.Sdk.Messages;
+
+namespace Microsoft.Xrm.Sdk
+{
+    internal static class ExecuteMultipleResponseExtensions
+    {
+        //Case 1: bulk create => may need id of each record
+        //Case 2: bulk update or delete => need to know only failures with record id's
+        //Case 3: ???
+
+        // Experimental
+        public static OrganizationRequestCollection GetRequests(this ExecuteMultipleResponse response)
+        {
+            return response.Results.GetValue<OrganizationRequestCollection>("Requests");
+        }
+
+        // Experimental
+        public static OrganizationRequest GetRequest(this ExecuteMultipleResponse response, ExecuteMultipleResponseItem item)
+        {
+            return response.GetRequests()?[item.RequestIndex];
+        }
+    }
+}
--- D365Extensions/D365Extensions/IOrganizationServiceExecuteExtensions.cs
+++ D365Extensions/D365Extensions/IOrganizationServiceExecuteExtensions.cs
@@ -68,21 +68,5 @@ namespace Microsoft.Xrm.Sdk
                 yield return response;
             }
         }
-
-        //Case 1: bulk create => may need id of each record
-        //Case 2: bulk update or delete => need to know only failures with record id's
-        //Case 3: ???
-
-        // Experimental
-        public static OrganizationRequestCollection GetRequests(this ExecuteMultipleResponse response)
-        {
-            return response.Results.GetValue<OrganizationRequestCollection>("Requests");
-        }
-
-        // Experimental
-        public static OrganizationRequest GetRequest(this ExecuteMultipleResponse response, ExecuteMultipleResponseItem item)
-        {
-            return response.GetRequests()?[item.RequestIndex];
-        }
     }
 }
