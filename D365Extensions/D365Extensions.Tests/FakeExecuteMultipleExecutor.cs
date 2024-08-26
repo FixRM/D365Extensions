ca6cb7d23bb21159de1e97e8815997a4d24b811b
Artem Gruinin <FixRM@users.noreply.github.com>
2024-08-26 18:21:05 +0500

    test: add FakeExecuteMultipleExecutor to simplify further testing

---
+++ D365Extensions/D365Extensions.Tests/FakeExecuteMultipleExecutor.cs
@@ -0,0 +1,114 @@
+﻿using FakeXrmEasy;
+using FakeXrmEasy.FakeMessageExecutors;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    /// <summary>
+    /// To fake OOB behavior
+    /// https://learn.microsoft.com/en-us/power-apps/developer/data-platform/org-service/execute-multiple-requests
+    /// </summary>
+    internal class FakeExecuteMultipleExecutor : IFakeMessageExecutor
+    {
+        private const string NameOfFaled = "IShouldFail";
+
+        private const string ResponceIdKey = "ResponseId";
+
+        public static OrganizationRequest GoodRequest => new OrganizationRequest() { RequestId = Guid.NewGuid() };
+
+        public static OrganizationRequest FailRequest => new OrganizationRequest() { RequestId = Guid.NewGuid(), RequestName = NameOfFaled };
+        
+        public List<ExecuteMultipleRequest> ActualRequests { get; } = new List<ExecuteMultipleRequest>();
+
+        public List<ExecuteMultipleResponse> Responses { get; } = new List<ExecuteMultipleResponse>();
+
+        public static Guid GetRequestId(ExecuteMultipleResponseItem item)
+        {
+            if (item.IsFaulted())
+            {
+                return Guid.Parse(item.Fault.Message);
+            }
+
+            return (Guid)item.Response[ResponceIdKey];
+        }
+
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is ExecuteMultipleRequest;
+        }
+
+        public bool IsFailed(OrganizationRequest orgRequest)
+        {
+            return orgRequest.RequestName == NameOfFaled;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
+        {
+            var eMultipleRequest = request as ExecuteMultipleRequest;
+            ActualRequests.Add(eMultipleRequest);
+
+            var eMultipleResponce = new ExecuteMultipleResponse()
+            {
+                [nameof(ExecuteMultipleResponse.Responses)] = new ExecuteMultipleResponseItemCollection()
+            };
+            Responses.Add(eMultipleResponce);
+
+            for (var i = 0; i < eMultipleRequest.Requests.Count; i++)
+            {
+                var orgRequest = eMultipleRequest.Requests[i];
+
+                if (!IsFailed(orgRequest))
+                {
+                    //should return good responses
+                    if (eMultipleRequest.Settings.ReturnResponses)
+                    {
+                        //add response to results
+                        eMultipleResponce.Responses.Add(new ExecuteMultipleResponseItem
+                        {
+                            RequestIndex = i,
+                            Response = new OrganizationResponse()
+                            {
+                                //to simplify request-responce mathing in a test
+                                [ResponceIdKey] = orgRequest.RequestId
+                            }
+                        });
+                    }
+                }
+                else
+                {
+                    //should always return errors
+                    eMultipleResponce.Responses.Add(new ExecuteMultipleResponseItem
+                    {
+                        RequestIndex = i,
+                        Fault = new OrganizationServiceFault()
+                        {
+                            //to simplify request-responce mathing in a test
+                            Message = orgRequest.RequestId.ToString()
+                        }
+                    });
+
+                    //should mark whole ExecuteMultipleResponse as faulted
+                    eMultipleResponce[nameof(ExecuteMultipleResponse.IsFaulted)] = true;
+
+                    // Should stop fake execution
+                    if (!eMultipleRequest.Settings.ContinueOnError)
+                        break;
+                }
+            }
+
+            return eMultipleResponce;
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(ExecuteMultipleRequest);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions.Tests/FakeExecuteMultipleExecutorTests.cs
@@ -0,0 +1,157 @@
+﻿#pragma warning disable CS0618 // Type or member is obsolete
+
+using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    [TestClass]
+    public class FakeExecuteMultipleExecutorTests
+    {
+        //https://learn.microsoft.com/en-us/power-apps/developer/data-platform/org-service/execute-multiple-requests
+        [DataTestMethod]
+        [DataRow(true, true, 6, 2, DisplayName = "ContinueOnError=true, ReturnResponses=true 6 response items: 2 have Fault set to a value.")]
+        [DataRow(false, true, 3, 1, DisplayName = "ContinueOnError=false, ReturnResponses=true 3 response items: 1 has Fault set to a value.")]
+        [DataRow(true, false, 2, 2, DisplayName = "ContinueOnError=true, ReturnResponses=false 2 response items: 2 have Fault set to a value.")]
+        [DataRow(false, false, 1, 1, DisplayName = "ContinueOnError=false, ReturnResponses=false 1 response item: 1 has Fault set to a value.")]
+        public void ExecuteTest(bool continueOnError, bool returnResponses, int expectedResponsesCount, int expectedErrorCount)
+        {
+            //Setup
+            var requests = new OrganizationRequestCollection()
+            {
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.FailRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.FailRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+            };
+
+            var settings = new ExecuteMultipleSettings()
+            {
+                ContinueOnError = continueOnError,
+                ReturnResponses = returnResponses,
+            };
+
+            var eMultipleRequest = new ExecuteMultipleRequest()
+            {
+                Settings = settings,
+                Requests = requests
+            };
+
+            //Act
+            var executor = new FakeExecuteMultipleExecutor();
+            var responce = executor.Execute(eMultipleRequest, null) as ExecuteMultipleResponse;
+
+            //Assert
+            Assert.AreEqual(expectedResponsesCount, responce.Responses.Count);
+            Assert.AreEqual(expectedErrorCount, responce.GetFaultedResponses().Count);
+            Assert.IsTrue(responce.IsFaulted);
+        }
+
+        [TestMethod]
+        public void MiscTest()
+        {
+            //Setup
+            var requests = new OrganizationRequestCollection()
+            {
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+            };
+
+            var settings = new ExecuteMultipleSettings()
+            {
+                ContinueOnError = true,
+                ReturnResponses = true,
+            };
+
+            var eMultipleRequest = new ExecuteMultipleRequest()
+            {
+                Settings = settings,
+                Requests = requests
+            };
+
+            //Act
+            var executor = new FakeExecuteMultipleExecutor();
+            var responce = executor.Execute(eMultipleRequest, null) as ExecuteMultipleResponse;
+
+            //Assert
+            Assert.AreEqual(requests.Count, responce.Responses.Count);
+            Assert.AreEqual(0, responce.GetFaultedResponses().Count);
+
+            //Not faulted test
+            Assert.IsFalse(responce.IsFaulted);
+
+            //Actual request tracking test
+            Assert.AreEqual(1, executor.ActualRequests.Count);
+            Assert.AreEqual(eMultipleRequest, executor.ActualRequests[0]);
+
+            //Response traking test
+            Assert.AreEqual(1, executor.Responses.Count);
+            Assert.AreEqual(responce, executor.Responses[0]);
+
+            for (int i = 0; i < requests.Count; i++)
+            {
+                //Correct request index test
+                Assert.AreEqual(i, responce.Responses[i].RequestIndex);
+
+                //Good requests are coverted as expected test
+                Assert.AreEqual(requests[i].RequestId, FakeExecuteMultipleExecutor.GetRequestId(responce.Responses[i]));
+            }
+        }
+
+        [TestMethod]
+        public void FakeXrmEasyIntegrationTest()
+        {
+            //Setup
+            var requests = new OrganizationRequestCollection()
+            {
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.FailRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+                FakeExecuteMultipleExecutor.GoodRequest,
+            };
+
+            var settings = new ExecuteMultipleSettings()
+            {
+                ContinueOnError = true,
+                ReturnResponses = true,
+            };
+
+            var eMultipleRequest = new ExecuteMultipleRequest()
+            {
+                Settings = settings,
+                Requests = requests
+            };
+
+            var executor = new FakeExecuteMultipleExecutor();
+
+            var context = new XrmFakedContext();
+            context.AddFakeMessageExecutor<ExecuteMultipleRequest>(executor);
+
+            //Act
+            var service = context.GetOrganizationService();
+
+            var eMultipleResponce = service.Execute(eMultipleRequest) as ExecuteMultipleResponse;
+
+            //Assert
+            //Assert.AreEqual(1, executor.ActualRequests.Count);
+            //Assert.AreEqual(eMultipleRequest, executor.ActualRequests[0]);
+
+            Assert.AreEqual(requests.Count, eMultipleResponce.Responses.Count);
+        }
+    }
+}
