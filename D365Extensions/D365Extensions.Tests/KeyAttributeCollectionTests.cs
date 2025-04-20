0af1fece7d800baebce24d52f43a50c4ef0a61bf
Artem Gruinin <FixRM@users.noreply.github.com>
2025-04-20 17:04:05 +0500

    feat: add strongly typed version of the KeyAttributeCollection class

---
+++ D365Extensions/D365Extensions.Tests/KeyAttributeCollectionTests.cs
@@ -0,0 +1,36 @@
+﻿using D365Extensions.Tests.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Tests
+{
+    [TestClass()]
+    public class KeyAttributeCollectionTests
+    {
+        [TestMethod()]
+        public void KeyAttributeCollectionTest()
+        {
+            // Setup
+            var expectedKey = "name";
+            var expectedValue = "FixRM";
+
+            var keys = new KeyAttributeCollection<Account>
+            {
+                { a => a.Name, expectedValue}
+            };
+
+            // Act
+            KeyAttributeCollection actualKeys = keys;
+            var kv = actualKeys.Single();
+
+            // Assert
+            Assert.AreEqual(expectedKey, kv.Key);
+            Assert.AreEqual(expectedValue, kv.Value);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/KeyAttributeCollection.cs
@@ -0,0 +1,30 @@
+﻿using D365Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk
+{
+    /// <summary>
+    /// Strongly typed version of the KeyAttributeCollection class
+    /// </summary>
+    public sealed class KeyAttributeCollection<T> : DataCollection<Expression<Func<T,object>>, object> where T : Entity
+    {
+        public static implicit operator KeyAttributeCollection(KeyAttributeCollection<T> t)
+        {
+            if (t == null) return null;
+
+            var keys = new KeyAttributeCollection();
+            
+            foreach (var kv in t)
+            {
+                keys.Add(LogicalName.GetName(kv.Key), kv.Value);
+            }
+
+            return keys;
+        }
+    }
+}
