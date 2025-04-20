d339382597868e864fd24e006c021075a2ee9e2a
Artem Gruinin <FixRM@users.noreply.github.com>
2025-04-20 16:49:51 +0500

    feat: add strongly typed version of the EntityReference class

---
+++ D365Extensions/D365Extensions.Tests/EntityReferenceTests.cs
@@ -0,0 +1,149 @@
+﻿using D365Extensions.Tests.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Tests
+{
+    [TestClass()]
+    public class EntityReferenceTests
+    {
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            // Act
+            EntityReference reference = new EntityReference<Account>();
+
+            // Assert
+            Assert.AreEqual(Account.EntityLogicalName, reference.LogicalName);
+        }
+
+        [TestMethod()]
+        public void Constructor2Test()
+        {
+            // Setup
+            var expectedId = Guid.NewGuid();
+
+            // Act
+            EntityReference reference = new EntityReference<Account>(expectedId);
+
+            // Assert
+            Assert.AreEqual(Account.EntityLogicalName, reference.LogicalName);
+            Assert.AreEqual(expectedId, reference.Id);
+        }
+
+        [TestMethod()]
+        public void Constructor3Test()
+        {
+            // Setup
+            var expectedKeys = new KeyAttributeCollection();
+
+            // Act
+            EntityReference reference = new EntityReference<Account>(expectedKeys);
+
+            // Assert
+            Assert.AreEqual(Account.EntityLogicalName, reference.LogicalName);
+            Assert.AreEqual(expectedKeys, reference.KeyAttributes);
+        }
+
+        [TestMethod()]
+        public void Constructor4Test()
+        {
+            // Setup
+            var expectedKey = "accountnumber";
+            var expectedValue = "123";
+
+            // Act
+            EntityReference reference = new EntityReference<Account>(expectedKey, expectedValue);
+            var actualKey = reference.KeyAttributes.Single();
+
+            // Assert
+            Assert.AreEqual(Account.EntityLogicalName, reference.LogicalName);
+            Assert.AreEqual(expectedKey, actualKey.Key);
+            Assert.AreEqual(expectedValue, actualKey.Value);
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            // Setup
+            var id = Guid.NewGuid();
+            var rowVersion = "123";
+            var name = "FixRM";
+            var key = "accountnumber";
+            var value = "42";
+
+            var reference1 = new EntityReference()
+            {
+                Id = id,
+                Name = name,
+                LogicalName = Account.EntityLogicalName,
+                RowVersion = rowVersion,
+                KeyAttributes =
+                {
+                    { key, value }
+                }
+            };
+
+            var reference2 = new EntityReference<Account>()
+            {
+                Id = id,
+                Name = name,
+                RowVersion = rowVersion,
+                KeyAttributes =
+                {
+                    { key, value }
+                }
+            };
+
+            // Act + Assert
+            Assert.IsTrue(reference1.Equals((EntityReference) reference2));
+            Assert.IsTrue(reference2.Equals(reference1));
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            // Setup
+            var id = Guid.NewGuid();
+            var rowVersion = "123";
+            var name = "FixRM";
+            var key = "accountnumber";
+            var value = "42";
+
+            var reference1 = new EntityReference()
+            {
+                Id = id,
+                Name = name,
+                LogicalName = Account.EntityLogicalName,
+                RowVersion = rowVersion,
+                KeyAttributes =
+                {
+                    { key, value }
+                }
+            };
+
+            var reference2 = new EntityReference<Account>()
+            {
+                Id = id,
+                Name = name,
+                RowVersion = rowVersion,
+                KeyAttributes =
+                {
+                    { key, value }
+                }
+            };
+
+            // Act
+            var code1 = reference1.GetHashCode();
+            var code2 = reference2.GetHashCode();
+
+            // Assert
+            Assert.AreEqual(code1, code2);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/EntityReference.cs
@@ -0,0 +1,129 @@
+﻿using D365Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk
+{
+    /// <summary>
+    /// Strongly typed version of the EntityReference class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class EntityReference<T> where T : Entity
+    {
+        /// <summary>
+        /// EntityReference has complex overrides of Equals and GetHashCode
+        /// So unlike other strongly typed versions in this library, it's esier to have this one to be
+        /// a wrapper of OOB type insted of independent implementation
+        /// </summary>
+        private readonly EntityReference reference;
+
+        public EntityReference()
+        {
+            this.reference = new EntityReference(D365Extensions.LogicalName.GetName<T>());
+        }
+
+        public EntityReference(Guid id)
+        {
+            this.reference = new EntityReference()
+            {
+                Id = id,
+                LogicalName = D365Extensions.LogicalName.GetName<T>()
+            };
+        }
+
+        public EntityReference(KeyAttributeCollection keyAttributes)
+        {
+            this.reference = new EntityReference()
+            {
+                LogicalName = D365Extensions.LogicalName.GetName<T>(),
+                KeyAttributes = keyAttributes
+            };
+        }
+
+        public EntityReference(string keyName, object keyValue)
+        {
+            this.reference = new EntityReference()
+            {
+                LogicalName = D365Extensions.LogicalName.GetName<T>(),
+                KeyAttributes = 
+                { 
+                    { keyName, keyValue } 
+                }
+            };
+        }
+
+        /// <summary>
+        /// Gets or sets the ID of the record
+        /// </summary>
+        public Guid Id 
+        { 
+            get => reference.Id; 
+            set => reference.Id = value; 
+        }
+
+
+        /// <summary>
+        /// Gets or sets the logical name of the entity.
+        /// </summary>
+        public string LogicalName 
+        { 
+            get => reference.LogicalName;            
+        }
+
+
+        /// <summary>
+        /// Gets or sets the value of the primary attribute of the entity.
+        /// </summary>
+        public string Name 
+        { 
+            get => reference.Name; 
+            set => reference.Name = value; 
+        }
+
+
+        /// <summary>
+        /// Gets or sets the key attributes.
+        /// </summary>
+        public KeyAttributeCollection KeyAttributes 
+        { 
+            get => reference.KeyAttributes; 
+            set => reference.KeyAttributes = value; 
+        }
+
+        /// <summary>
+        /// Gets or sets the row version.
+        /// </summary>
+        public string RowVersion 
+        { 
+            get => reference.RowVersion; 
+            set => reference.RowVersion = value; 
+        }
+
+        /// <summary>
+        /// WARNING: EntityReference and EntityReference<T> are different types! You can't compare them directly!
+        /// You should always explicitly cast EntityReference<T> to EntityReference before comparison:
+        /// <code>
+        /// reference1.Equals((EntityReference) reference2)
+        /// </code>
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            return reference.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return reference.GetHashCode();
+        }
+
+        public static implicit operator EntityReference(EntityReference<T> t)
+        {
+            if (t == null) return null;
+
+            return t.reference;
+        }
+    }
+}
