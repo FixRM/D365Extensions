4d35791b71078c25765746ada21bf80261a096e0
Artem Gruinin <FixRM@users.noreply.github.com>
2025-05-02 16:29:49 +0500

    feat: add generic value of AliasedValue

---
+++ D365Extensions/D365Extensions.Tests/AliasedValueTests.cs
@@ -0,0 +1,45 @@
+﻿using D365Extensions.Tests.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    [TestClass()]
+    public class AliasedValueTests
+    {
+        [TestMethod()]
+        public void AliasedValueTest()
+        {
+            // Setup
+            var expectedEntityName = "account";
+            var expectedAttributeName = "accountnumber";
+            var expectedValue = "42";
+
+            // Act
+            AliasedValue aliasedValue = new AliasedValue<Account>(a=> a.AccountNumber, expectedValue);
+
+            // Assert
+            Assert.AreEqual(expectedEntityName, aliasedValue.EntityLogicalName);
+            Assert.AreEqual(expectedAttributeName, aliasedValue.AttributeLogicalName);
+            Assert.AreEqual(expectedValue, aliasedValue.Value);
+        }
+
+        [TestMethod()]
+        public void NullTest()
+        {
+            // Setup
+            AliasedValue<Account> aliasedValueT = null;
+
+            // Act
+            AliasedValue aliasedValue = aliasedValueT;
+
+            // Assert
+            Assert.IsNull(aliasedValue);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/AliasedValue.cs
@@ -0,0 +1,33 @@
+﻿using D365Extensions;
+using System;
+using System.Linq.Expressions;
+
+namespace Microsoft.Xrm.Sdk
+{
+    /// <summary>
+    /// Generic version if AliasedValue class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class AliasedValue<T> where T : Entity
+    {
+        public Expression<Func<T, object>> AttributeName { get; }
+
+        public object Value { get; }
+
+        public AliasedValue(Expression<Func<T, object>> attributeName, object value)
+        {
+            AttributeName = attributeName;
+            Value = value;
+        }
+
+        public static implicit operator AliasedValue(AliasedValue<T> t)
+        {
+            if (t is null) return null;
+
+            return new AliasedValue(
+                entityLogicalName: LogicalName.GetName<T>(),
+                attributeLogicalName: LogicalName.GetName(t.AttributeName),
+                value: t.Value);
+        }
+    }
+}
