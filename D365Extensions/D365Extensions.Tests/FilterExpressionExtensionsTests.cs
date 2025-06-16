7e6c6d761d8ea852fd940fd99a882dfcf5b1f0ee
Artem Gruinin <FixRM@users.noreply.github.com>
2025-06-16 18:29:21 +0500

    hotfix: AddCondition<T> now don't assign EntityName property

--- D365Extensions/D365Extensions.Tests/FilterExpressionExtensionsTests.cs
+++ D365Extensions/D365Extensions.Tests/FilterExpressionExtensionsTests.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using D365Extensions.Tests.Entities;
+using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task = D365Extensions.Tests.Entities.Task;
 
 namespace D365Extensions.Tests
 {
@@ -26,11 +29,11 @@ namespace D365Extensions.Tests
 
             // Assert
             Assert.AreEqual(1, filter.Conditions.Count);
-            ConditionExpression actualtCondition = filter.Conditions[0];
+            ConditionExpression actualCondition = filter.Conditions[0];
 
-            Assert.AreEqual(expectedAttribute, actualtCondition.AttributeName);
-            Assert.AreEqual(expectedOperator, actualtCondition.Operator);
-            Assert.AreEqual(expectedValue, actualtCondition.Values[0]);
+            Assert.AreEqual(expectedAttribute, actualCondition.AttributeName);
+            Assert.AreEqual(expectedOperator, actualCondition.Operator);
+            Assert.AreEqual(expectedValue, actualCondition.Values[0]);
         }
 
         [TestMethod()]
@@ -39,7 +42,6 @@ namespace D365Extensions.Tests
             // Setup
             FilterExpression filter = new FilterExpression();
 
-            var expectedEntityName = TestEntity.EntityLogicalName;
             var expectedAttribute = nameof(TestEntity.ReferenceTypeProperty).ToLower();
             var expectedOperator = ConditionOperator.Equal;
             var expectedValue = "not used";
@@ -49,12 +51,43 @@ namespace D365Extensions.Tests
 
             // Assert
             Assert.AreEqual(1, filter.Conditions.Count);
-            ConditionExpression actualtCondition = filter.Conditions[0];
+            ConditionExpression actualCondition = filter.Conditions[0];
 
-            Assert.AreEqual(expectedAttribute, actualtCondition.AttributeName);
-            Assert.AreEqual(expectedOperator, actualtCondition.Operator);
-            Assert.AreEqual(expectedValue, actualtCondition.Values[0]);
-            Assert.AreEqual(expectedEntityName, actualtCondition.EntityName);
+            Assert.AreEqual(expectedAttribute, actualCondition.AttributeName);
+            Assert.AreEqual(expectedOperator, actualCondition.Operator);
+            Assert.AreEqual(expectedValue, actualCondition.Values[0]);
+        }
+
+        [TestMethod()]
+        public void QueryTest()
+        {
+            var account = new Account()
+            {
+                Id = Guid.NewGuid(),
+            };
+
+            var task = new Task()
+            {
+                Id = Guid.NewGuid(),
+                RegardingObjectId = account.ToEntityReference(),
+                Description = "Test"
+            };
+
+            var query = new QueryExpression(Account.EntityLogicalName);
+
+            var link = query.AddLink<Account, Task>(
+                a => a.AccountId,
+                p => p.RegardingObjectId,
+                JoinOperator.Inner);
+
+            link.LinkCriteria.AddCondition<Task>(p => p.Description, ConditionOperator.Equal, "Test");
+            
+            var context = new XrmFakedContext();
+            context.Initialize([account, task]);
+
+            var service = context.GetOrganizationService();
+
+            var results = service.RetrieveMultiple(query);
         }
     }
 }
