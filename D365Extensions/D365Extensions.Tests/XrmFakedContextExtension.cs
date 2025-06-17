56c0480096e1839e25af99d2fe55daa95baa4821
Artem Gruinin <FixRM@users.noreply.github.com>
2025-06-17 11:47:46 +0500

    tests: add helper method

---
+++ D365Extensions/D365Extensions.Tests/XrmFakedContextExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk.Metadata;
+
+namespace FakeXrmEasy.Extensions
+{
+    public static class XrmFakedContextExtension
+    {
+        public static void InitializeKeyMetadata(this XrmFakedContext context, string entityLogicalName, string[] keyAttributeNames)
+        {
+            var alternateKeyMetadata = new EntityKeyMetadata
+            {
+                KeyAttributes = keyAttributeNames
+            };
+
+            var entityMetadata = new EntityMetadata
+            {
+                LogicalName = entityLogicalName
+            };
+
+            entityMetadata.SetFieldValue("_keys", new EntityKeyMetadata[]
+            {
+                alternateKeyMetadata
+            });
+
+            context.InitializeMetadata(entityMetadata);
+        }
+    }
+}
