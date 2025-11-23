9580655763b5a8bba5e6fbfb7b25d5f14555a68e
Artem Gruinin <FixRM@users.noreply.github.com>
2025-11-23 17:20:50 +0500

    feat: add QueryByAttribute<T>

---
+++ D365Extensions/D365Extensions.Tests/QueryByAttributeTests.cs
@@ -0,0 +1,101 @@
+﻿using D365Extensions.Tests.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Extensions.Tests
+{
+    [TestClass()]
+    public class QueryByAttributeTests
+    {
+        [TestMethod()]
+        public void CastTest()
+        {
+            // Setup
+            var query = new QueryByAttribute<Account>()
+            {
+                TopCount = 1,
+                PageInfo = new PagingInfo(),
+                ColumnSet = new ColumnSet<Account>(a => a.Name),
+                Orders =
+                {
+                    new OrderExpression<Account>(a=> a.Name, OrderType.Ascending)
+                },
+                AttributeValues =
+                {
+                    { a=> a.AccountNumber, "42" },
+                    { a=> a.Name, "FixRM" }
+                }
+            };
+
+            // Act
+            QueryByAttribute queryByAttribute = query;
+
+            // Assert
+            Assert.AreEqual(Account.EntityLogicalName, queryByAttribute.EntityName);
+            Assert.AreEqual(query.TopCount, queryByAttribute.TopCount);
+            Assert.AreEqual(query.PageInfo, queryByAttribute.PageInfo);
+            CollectionAssert.AreEqual(query.Orders, queryByAttribute.Orders);
+
+            Assert.AreEqual(2, queryByAttribute.Attributes.Count);
+            Assert.AreEqual("accountnumber", queryByAttribute.Attributes[0]);
+            Assert.AreEqual("name", queryByAttribute.Attributes[1]);
+            Assert.AreEqual(2, queryByAttribute.Values.Count);
+
+            Assert.AreEqual("42", queryByAttribute.Values[0]);
+            Assert.AreEqual("FixRM", queryByAttribute.Values[1]);
+        }
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            //Setup
+            var query = new QueryByAttribute<Account>();
+
+            //Act
+            query.AddOrder(a => a.Name, OrderType.Descending);
+
+            //Assert
+            Assert.AreEqual(1, query.Orders.Count);
+            Assert.AreEqual("name", query.Orders[0].AttributeName);
+            Assert.AreEqual(OrderType.Descending, query.Orders[0].OrderType);
+        }
+
+        [TestMethod()]
+        public void AddAttributeValueTest()
+        {
+            //Setup
+            Expression<Func<Account, object>> expectedAttributeName = a => a.Name;
+            var expectedValue = "FixRM";
+
+            var query = new QueryByAttribute<Account>();
+
+            //Act
+            query.AddAttributeValue(a => a.Name, expectedValue);
+
+            //Assert
+            Assert.AreEqual(1, query.AttributeValues.Count);
+            Assert.AreEqual(expectedAttributeName.ToString(), query.AttributeValues.Keys.First().ToString());
+            Assert.AreEqual(expectedValue, query.AttributeValues.Values.First());
+        }
+
+        [TestMethod()]
+        public void NullCastTest()
+        {
+            //Setup
+            QueryByAttribute<Account> query = null;
+
+            //Act
+            QueryByAttribute queryByAttribute = query;
+
+            //Assert
+            Assert.IsNull(queryByAttribute);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/QueryByAttribute.cs
@@ -0,0 +1,85 @@
+﻿using D365Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Query
+{
+    public sealed class QueryByAttribute<T> where T : Entity
+    {
+        /// <summary>
+        /// Gets the set of attributes selected in the query and values to look for when the query is executed
+        /// </summary>
+        public Dictionary<Expression<Func<T, object>>, object> AttributeValues { get; private set; } = new Dictionary<Expression<Func<T, object>>, object>();
+
+        /// <summary>
+        /// Gets or sets the number of pages and the number of entity instances per page
+        /// returned from the query
+        /// </summary>
+        public PagingInfo PageInfo { get; set; } = new PagingInfo();
+
+        /// <summary>
+        /// Gets or sets the column set
+        /// </summary>
+        public ColumnSet ColumnSet { get; set; } = new ColumnSet();
+
+        /// <summary>
+        /// Gets the order in which the entity instances are returned from the query
+        /// </summary>
+        public List<OrderExpression> Orders { get; private set; } = new List<OrderExpression>();
+
+        /// <summary>
+        /// Gets or sets the number of rows to be returned
+        /// 
+        /// When specified, this limits the number rows returned in a query result set to
+        /// the specified number of rows.
+        ///
+        /// A query can contain either Microsoft.Xrm.Sdk.Query.QueryByAttribute.PageInfo
+        /// or Microsoft.Xrm.Sdk.Query.QueryByAttribute.TopCount property values. If both
+        /// are specified, an error will be thrown
+        /// </summary>
+        public int? TopCount { get; set; }
+
+        /// <summary>
+        /// Adds an order to the orders collection
+        /// </summary>
+        /// <param name="attributeName">The logical name of the attribute</param>
+        /// <param name="orderType">The order for that attribute</param>
+        public void AddOrder(Expression<Func<T, object>> attributeName, OrderType orderType)
+        {
+            Orders.Add(new OrderExpression<T>(attributeName, orderType));
+        }
+
+        /// <summary>
+        /// Adds an attribute value to the attributes collection
+        /// </summary>
+        /// <param name="attributeName">The logical name of the attribute</param>
+        /// <param name="value">The attribute value</param>
+        public void AddAttributeValue(Expression<Func<T, object>> attributeName, object value)
+        {
+            this.AttributeValues.Add(attributeName, value);
+        }
+
+        public static implicit operator QueryByAttribute(QueryByAttribute<T> q)
+        {
+            if (q is null) return null;
+
+            var queryByAttribute = new QueryByAttribute()
+            {
+                EntityName = LogicalName.GetName<T>(),
+                ColumnSet = q.ColumnSet,
+                TopCount = q.TopCount,
+                PageInfo = q.PageInfo,
+            };
+            queryByAttribute.Attributes.AddRange(LogicalName.GetNames(q.AttributeValues.Keys));
+            queryByAttribute.Values.AddRange(q.AttributeValues.Values);
+            queryByAttribute.Orders.AddRange(q.Orders);
+
+            return queryByAttribute;
+        }
+    }
+}
