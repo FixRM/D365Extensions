585a8dac104af08566e2279d9a7912d0b9eca5f2
Artem Gruinin <FixRM@users.noreply.github.com>
2025-11-24 14:18:32 +0500

    feat: add QueryExpression<T>

---
+++ D365Extensions/D365Extensions.Tests/QueryExpressionTests.cs
@@ -0,0 +1,99 @@
+﻿using D365Extensions.Tests.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace D365Extensions.Tests
+{
+    [TestClass()]
+    public class QueryExpressionTests
+    {
+        [TestMethod()]
+        public void CastTest()
+        {
+            //Setup
+            var query = new QueryExpression<Account>()
+            {
+                TopCount = 1,
+                PageInfo = new PagingInfo(),
+                Criteria = new FilterExpression(),
+                Distinct = true,
+                ColumnSet = new ColumnSet<Account>(a => a.Name),
+                NoLock = true,
+                LinkEntities =
+                {
+                    new LinkEntity<Account, Task>(a=> a.AccountId, t=> t.RegardingObjectId, JoinOperator.Inner)
+                },
+                Orders =
+                {
+                    new OrderExpression<Account>(a=> a.CreatedOn, OrderType.Descending)
+                }
+            };
+
+            //Act
+            QueryExpression queryExpression = query;
+
+            //Assert
+            Assert.AreEqual(Account.EntityLogicalName, queryExpression.EntityName);
+            Assert.AreEqual(query.TopCount, queryExpression.TopCount);
+            Assert.AreEqual(query.PageInfo, queryExpression.PageInfo);
+            Assert.AreEqual(query.Criteria, queryExpression.Criteria);
+            Assert.AreEqual(query.Distinct, queryExpression.Distinct);
+            Assert.AreEqual(query.ColumnSet, queryExpression.ColumnSet);
+            Assert.AreEqual(query.NoLock, queryExpression.NoLock);
+            CollectionAssert.AreEqual(query.LinkEntities, queryExpression.LinkEntities);
+            CollectionAssert.AreEqual(query.Orders, queryExpression.Orders);
+        }
+
+        [TestMethod()]
+        public void NullCastTest()
+        {
+            //Setup
+            QueryExpression<Account> query = null;
+
+            //Act
+            QueryExpression queryExpression = query;
+
+            //Assert
+            Assert.IsNull(queryExpression);
+        }
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            //Setup
+            var query = new QueryExpression<Account>();
+
+            //Act
+            query.AddOrder(a=> a.Name, OrderType.Descending);
+
+            //Assert
+            Assert.AreEqual(1, query.Orders.Count);
+            Assert.AreEqual("name", query.Orders[0].AttributeName);
+            Assert.AreEqual(OrderType.Descending, query.Orders[0].OrderType);
+        }
+
+        [TestMethod()]
+        public void AddLinkTest()
+        {
+            //Setup
+            var query = new QueryExpression<Account>();
+
+            //Act
+            var link = query.AddLink<Account, Task>(a=> a.AccountId, t=> t.RegardingObjectId, JoinOperator.Inner);
+
+            //Assert
+            Assert.AreEqual(1, query.LinkEntities.Count);
+            Assert.AreEqual(link, query.LinkEntities[0]);
+            Assert.AreEqual(Account.EntityLogicalName, query.LinkEntities[0].LinkFromEntityName);
+            Assert.AreEqual("accountid", query.LinkEntities[0].LinkFromAttributeName);
+            Assert.AreEqual(Task.EntityLogicalName, query.LinkEntities[0].LinkToEntityName);
+            Assert.AreEqual("regardingobjectid", query.LinkEntities[0].LinkToAttributeName);
+            Assert.AreEqual(JoinOperator.Inner, query.LinkEntities[0].JoinOperator);
+        }
+    }
+}
---
+++ D365Extensions/D365Extensions/QueryExpression.cs
@@ -0,0 +1,154 @@
+﻿using D365Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xrm.Sdk.Query
+{
+    public class QueryExpression<T> where T : Entity
+    {
+        /// <summary>
+        /// Gets or sets the number of rows to be returned.
+        ///
+        /// When specified, this limits the number rows returned in a query result set to
+        /// the specified number of rows.
+        ///
+        /// A query can contain either Microsoft.Xrm.Sdk.Query.QueryExpression.PageInfo or
+        /// Microsoft.Xrm.Sdk.Query.QueryExpression.TopCount property values. If both are
+        /// specified, an error will be thrown.
+        /// </summary>
+        public int? TopCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of pages and the number of entity instances per page
+        /// returned from the query.
+        ///
+        /// A query can contain either Microsoft.Xrm.Sdk.Query.QueryExpression.PageInfo or
+        /// Microsoft.Xrm.Sdk.Query.QueryExpression.TopCount property values. If both are
+        /// specified, an error will be thrown.
+        /// </summary>
+        public PagingInfo PageInfo { get; set; } = new PagingInfo();
+
+        /// <summary>
+        /// Gets or sets the columns to include.
+        /// </summary>
+        public ColumnSet ColumnSet { get; set; } = new ColumnSet();
+
+        /// <summary>
+        /// Gets or sets the complex condition and logical filter expressions that filter
+        /// </summary>
+        public FilterExpression Criteria { get; set; } = new FilterExpression();
+
+        /// <summary>
+        ///  Gets or sets whether the results of the query contain duplicate entity instances.
+        /// </summary>
+        public bool Distinct { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value that indicates that no shared locks are issued against the
+        /// data that would prohibit other transactions from modifying the data in the records
+        /// returned from the query.
+        ///
+        /// The benefit of setting NoLock to true is that it allows you to keep the system
+        /// from issuing locks against the entities in your queries; this increases concurrency
+        /// and performance because the database engine does not have to maintain the shared
+        /// locks involved. The downside is that, because no locks are issued against the
+        /// records being read, some "dirty” or uncommitted data could potentially be read.
+        /// A "dirty" read is one in which the data being read is involved in a transaction
+        /// from another connection. If that transaction rolls back its work, the data read
+        /// from the query using NoLock will have read uncommitted data. This type of read
+        /// makes processing inconsistent and can lead to problems.
+        /// </summary>
+        public bool NoLock { get; set; }
+
+        /// <summary>
+        /// Gets a collection of the links between multiple entity types.
+        /// </summary>
+        public List<LinkEntity> LinkEntities { get; private set; } = new List<LinkEntity>();
+
+        /// <summary>
+        /// Gets the order in which the entity instances are returned from the query.
+        /// </summary>
+        public List<OrderExpression> Orders { get; private set; } = new List<OrderExpression>();
+
+        public static implicit operator QueryExpression(QueryExpression<T> q)
+        {
+            if (q is null) return null;
+
+            var queryExpression = new QueryExpression()
+            {
+                EntityName = LogicalName.GetName<T>(),
+                TopCount = q.TopCount,
+                PageInfo = q.PageInfo,
+                ColumnSet = q.ColumnSet,
+                Criteria = q.Criteria,
+                Distinct = q.Distinct,
+                NoLock = q.NoLock,
+            };
+            queryExpression.LinkEntities.AddRange(q.LinkEntities);
+            queryExpression.Orders.AddRange(q.Orders);
+
+            return queryExpression;
+        }
+
+        /// <summary>
+        /// Adds the specified order expression to the query expression.
+        /// </summary>
+        /// <param name="attributeName">The name of the attribute.</param>
+        /// <param name="orderType">The order type.</param>
+        public void AddOrder(Expression<Func<T, object>> attributeName, OrderType orderType)
+        {
+            Orders.Add(new OrderExpression(LogicalName.GetName(attributeName), orderType));
+        }
+
+        /// <summary>
+        /// Adds the specified link to the query expression setting the entity name to link
+        /// to, the attribute name to link from and the attribute name to link to.
+        /// </summary>
+        /// <typeparam name="TFrom">Type of the entity to link from</typeparam>
+        /// <typeparam name="TTo">Type of the entity to link to</typeparam>
+        /// <param name="linkFromAttributeName">The property expressions containing the name of the attribute to link from.</param>
+        /// <param name="linkToAttributeName">The property expressions containing the name of the attribute to link to.</param>
+        public LinkEntity AddLink<TFrom, TTo>(
+            Expression<Func<TFrom, object>> linkFromAttributeName,
+            Expression<Func<TTo, object>> linkToAttributeName)
+            where TFrom : Entity
+            where TTo : Entity
+        {
+            return AddLink(linkFromAttributeName, linkToAttributeName, JoinOperator.Inner);
+        }
+
+        /// <summary>
+        /// Adds the specified link to the query expression setting the entity name to link
+        /// to, the attribute name to link from and the attribute name to link to.
+        /// </summary>
+        /// <typeparam name="TFrom">Type of the entity to link from</typeparam>
+        /// <typeparam name="TTo">Type of the entity to link to</typeparam>
+        /// <param name="linkFromAttributeName">The property expressions containing the name of the attribute to link from.</param>
+        /// <param name="linkToAttributeName">The property expressions containing the name of the attribute to link to.</param>
+        /// <param name="joinOperator">The join operator.</param>
+        public LinkEntity AddLink<TFrom, TTo>(
+            Expression<Func<TFrom, object>> linkFromAttributeName,
+            Expression<Func<TTo, object>> linkToAttributeName,
+            JoinOperator joinOperator)
+            where TFrom : Entity
+            where TTo : Entity
+        {
+            var linkEntity = new LinkEntity()
+            {
+                LinkFromAttributeName = LogicalName.GetName(linkFromAttributeName),
+                LinkFromEntityName = LogicalName.GetName<TFrom>(),
+                LinkToAttributeName = LogicalName.GetName(linkToAttributeName),
+                LinkToEntityName = LogicalName.GetName<TTo>(),
+                JoinOperator = joinOperator,
+            };
+
+            LinkEntities.Add(linkEntity);
+
+            return linkEntity;
+        }
+    }
+}
